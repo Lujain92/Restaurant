27db4b9fc546096d08cccda599b90e78b2b83e4c
Lujain92 <lujainaljarrah92@gmail.com>
2022-08-09 00:33:54 +0300

    no

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+let getForm = document.getElementById("form");
+let getTable = document.getElementById("table");
+const Fruit = [];
 
-functon food (foodId,nameF,type,price){
-    this.foodId=foodId;
-    this.nameF=nameF;
-    this.type=type;
-    this.price=price;
-    
+
+function food(foodName, type, price) {
+  this.foodId=0;
+  this.foodName = foodName;
+  this.type = type;
+  this.price = price;
+  this.uniqueId =function (){
+    this.foodId+=foodId;
+    foodId++;
+    return this.foodId
+  }
+  Fruit.push(this);
 }
+
